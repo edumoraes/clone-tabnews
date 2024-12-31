4f96415e91fb46e83234cac2c2acd87135f6357f
Edu Moraes <edumoraesdg@gmail.com>
2024-12-31 12:47:01 +0000

    Adiciona as credenciais de conexão local do banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-export default function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   res.status(200).json({
     eduardo: "esse é meu nome",
   });
 }
+
+export default status;
