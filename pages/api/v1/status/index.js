41b9cfa0793450ffe8fd400ecb4a75aa9585f9e3
Edu Moraes <edumoraesdg@gmail.com>
2025-01-03 22:54:10 +0000

    Finaliza a implementação do endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,30 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  
+  const queryDatabaseVersion = await database.query("SHOW server_version;")
+  const databaseVersion = queryDatabaseVersion.rows[0].server_version
+  
+  const maxConnections = await database.query("SHOW max_connections;")
+  
+  const openedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+
   res.status(200).json({
-    eduardo: "esse é meu nome",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].count
+      }
+    }
   });
 }
 
