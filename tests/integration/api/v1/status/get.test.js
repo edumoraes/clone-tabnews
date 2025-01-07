dfd57399852fc6209ac85da685de50f3f76f018f
Edu Moraes <edumoraesdg@gmail.com>
2025-01-07 18:04:09 +0000

    remove code comented

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -14,7 +14,5 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.version).toEqual("16.6");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-  //expect(responseBody.database.max_connections).toBeDefined();
-  //expect(responseBody.database.opened_connections).toBeDefined();
 
 });
