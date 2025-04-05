5204e4106143ced817d26564749c353e75758f71
Eduardo de Moraes <edumoraesdg@gmail.com>
2025-04-05 18:26:16 -0400

    fix prettier stule linting

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -7,18 +7,17 @@ async function waitForAllServices() {
     return retry(fetchStatusPage, {
       retries: 100,
       maxTimeout: 1000,
-    })
+    });
   }
 
   async function fetchStatusPage() {
-    const response  = await fetch("http://localhost:3000/api/v1/status");
+    const response = await fetch("http://localhost:3000/api/v1/status");
     if (response.status !== 200) {
       throw Error();
     }
   }
-
 }
 
 export default {
   waitForAllServices,
-}
+};
